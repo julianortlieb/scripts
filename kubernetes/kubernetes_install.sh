56c43708a921f7f15aea06befc212eacae9d3f43
Julian Ortlieb <61797558+julianortlieb@users.noreply.github.com>
2024-03-08 15:48:34 +0100

    Update kubernetes_install.sh

--- kubernetes/kubernetes_install.sh
+++ kubernetes/kubernetes_install.sh
@@ -50,22 +50,25 @@ fi
 
 # ------- Docker -------
 # Install docker
-# Add Docker's official GPG key:
-apt-get update
-apt-get install ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
+# Check if Docker is already installed
+if ! command -v docker &> /dev/null; then
+    # Add Docker's official GPG key:
+    apt-get update
+    apt-get install ca-certificates curl
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
-echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
-    tee /etc/apt/sources.list.d/docker.list >/dev/null
-apt-get update
+    # Add the repository to Apt sources:
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+        tee /etc/apt/sources.list.d/docker.list >/dev/null
+    apt-get update
 
-# Install docker packages
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    # Install docker packages
+    apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
 
 # ------- Install CRI-Dockerd -------
 # Get latest deb-Package from github https://github.com/Mirantis/cri-dockerd/releases/
