eda8ae99dd396c50af23dc5b486ae1461ce18a07
julianortlieb <julian@julian-ortlieb.de>
2024-03-10 13:48:55 +0100

    Hinzufügen von Skript zur Aktivierung von WOL unter Linux

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Usefull Scripts
 
+## Linux
+### Enable WOL
+```bash
+curl -sSL https://raw.githubusercontent.com/julianortlieb/scripts/main/linux/wol_enable.sh | bash
+```
+
 ## Kubernetes
 ### Installation
 ```bash
---
+++ linux/wol_enable.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+###########################################################
+# TITLE: Enable Wake on LAN on debian based systems
+# DESCRIPTION: This script enables Wake on LAN on debian based systems
+# AUTHOR: Julian Ortlieb
+# DATE: 2022-03-10
+###########################################################
+
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Get possible interfaces
+interfaces=$(ip link show | grep -oP '^\d+:\s+\K[^:]+')
+
+# Ask the user for the interface to enable Wake on LAN with Whiptail
+interface=$(whiptail --title "Wake on LAN" --menu "Select the interface to enable Wake on LAN" 15 60 4 "${interfaces[@]}" 3>&1 1>&2 2>&3)
+
+# Check if the user has canceled the dialog
+if [ $? -eq 1 ]; then
+  echo "User canceled the dialog"
+  exit
+fi
+
+# Enable Wake on LAN
+ethtool -s $interface wol g
+
+# Check if the command was successful
+if [ $? -eq 0 ]; then
+  echo "Wake on LAN has been enabled"
+else
+  echo "An error occurred"
+fi
+
+# Exit the script
+exit
