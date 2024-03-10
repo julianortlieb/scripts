e0bc20249788a94f5371940ea3842d1b2ceffbf5
julianortlieb <julian@julian-ortlieb.de>
2024-03-10 19:25:58 +0100

    Aktiviere Wake on LAN für bestimmte Schnittstelle in Linux

--- linux/wol_enable.sh
+++ linux/wol_enable.sh
@@ -9,15 +9,17 @@
 
 # Check if the script is run as root
 if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+    then echo "Bitte als root ausführen"
+    exit
 fi
 
 # Get possible interfaces
-interfaces=$(ip link show | grep -oP '^\d+:\s+\K[^:]+')
+while IFS= read -r line; do
+    interfaces+=("$line")
+done < <(ip link show | grep -oP '^\d+:\s+\K[^:]+')
 
 # Ask the user for the interface to enable Wake on LAN with Whiptail
-interface=$(whiptail --title "Wake on LAN" --menu "Select the interface to enable Wake on LAN" 15 60 4 "${interfaces[@]}" 3>&1 1>&2 2>&3)
+interface=$(whiptail --title "Wake on LAN" --menu "Wählen Sie die Schnittstelle aus, um Wake on LAN zu aktivieren" 15 60 4 "${interfaces[@]}" 3>&1 1>&2 2>&3)
 
 # Check if the user has canceled the dialog
 if [ $? -eq 1 ]; then
@@ -37,8 +39,9 @@ fi
 
 # Ask the user if the wol should be enabled permanently
 if (whiptail --title "Wake on LAN" --yesno "Do you want to enable Wake on LAN permanently?" 10 60) then
-  # Add the command to the rc.local file
-  echo "ethtool -s $interface wol g" >> /etc/rc.local
+  # Add the post up command to the etc interfaces.
+  sed -i "/iface $interface inet static/a \ \ \ \ post-up ethtool -s $interface wol g" /etc/network/interfaces
+
   # Check if the command was successful
   if [ $? -eq 0 ]; then
     echo "Wake on LAN has been enabled permanently"
